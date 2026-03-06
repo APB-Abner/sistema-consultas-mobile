5b7333c779a6c34439b33f12376c8c9b20a6f9e8
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2026-03-05 22:20:23 -0300

    feat: update App component with custom title and background color

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulo}>3 ESR</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d91919',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 50,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
